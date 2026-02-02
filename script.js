420240348614edd05402d707d4f481ccf44b85c3
ledieptheanh28 <ledieptheanhst79@gmail.com>
2026-02-02 22:14:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -68,21 +68,5 @@ function addComment(text, list) {
   list.appendChild(commentDiv);
 }
  
-const backToTop = document.getElementById("backToTop");
-
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 200) {
-      backToTop.style.display = "block";
-    } else {
-      backToTop.style.display = "none";
-    }
-  });
-
-  backToTop.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  });
 
 
